ac02b0dfce96b794cf3d147ae67fb1bdee034dd9
Diana <dianaaranda14@gmail.com>
2023-06-15 11:36:17 -0700

    Part 2

--- melons.py
+++ melons.py
@@ -15,6 +15,11 @@ class AbstractMelonOrder:
         """Calculate price, including tax."""
 
         base_price = 5
+        if self.species == "christmas":
+            base_price = base_price * 1.5
+        if self.order_type == "international" and self.qty < 10: 
+            base_price += 3
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
